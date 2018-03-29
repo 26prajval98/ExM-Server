f7646d29ff446a14ecc1309eeac4350c9f39981e
26prajval98 <26prajval98@gmail.com>
2018-03-29 20:48:19 +0530

    emailNotificationDone

--- routes/toBuy.js
+++ routes/toBuy.js
@@ -4,10 +4,20 @@ var bodyParser = require('body-parser');
 var toBuy = require('../models/toBuy');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+var mailer = require('node-mailer');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'donotreplyexm@gmail.com',
+    pass: 'exm12345'
+  }
+});
 
 router.get('/', authenticate.verifyUser, (req,res,next)=>{
     toBuy.findOne({uid:req.user._id})
@@ -58,13 +68,39 @@ router.post('/delete', authenticate.verifyUser, (req,res,next)=>{
     })
 });
 
+var obj;
+
 router.post('/update', authenticate.verifyUser, (req,res,next)=>{
     toBuy.findOne({uid:req.user._id})
     .then((toBuyList)=>{
         toBuyList.toBuy.id(req.body.id).purchased = true;
+        obj = toBuyList.toBuy.id(req.body.id);
         return toBuyList.save();
     })
     .then(()=>{
+        var mailOptions = {
+            from: 'donotreplyexm@gmail.com',
+            to: req.user.username,
+            subject: 'Your Purchase',
+            html:`
+            <div style="width:100%;max-width:800px;position:absolute;">
+                <div style="margin:auto;position:relative;">
+                    <h1>Congratulations On Purchase</h1>
+                    <p>
+                        You have purchased ${obj.item} for INR
+                         ${obj.price} on ${new Date()}
+                    </p>
+                </div>
+            </div>           
+            `
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+        });
         res.json({Success:true});
     })
 });
